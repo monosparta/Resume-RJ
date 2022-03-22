656b7c6fd1a30a930bf3750c4c876baa53a19200
rj1110634028 <jo901025@gmail.com>
2022-03-22 12:52:38 +0800

    new Resume page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Resume from './page/Resume';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Resume />
   </React.StrictMode>,
   document.getElementById('root')
 );
