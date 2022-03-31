cc5119a02bcf6cf127627a06ff4d83de770150d3
Jonathan <jo901025@gmail.com>
2022-03-31 14:16:15 +0800

    rename pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Resume from './Page/Resume';
+import Resume from './Pages/Resume';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
