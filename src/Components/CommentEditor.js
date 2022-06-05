356699fe3810c4acf813a19339dfda89b8b280df
Jonathan <jo901025@gmail.com>
2022-06-05 17:04:00 +0800

    update comment list style

--- src/Components/CommentEditor.js
+++ src/Components/CommentEditor.js
@@ -7,32 +7,39 @@ import {
   Input,
   Card,
   Skeleton,
-  // Divider,
+  Space,
 } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import moment from "./LocaleMoment";
 import axios from "../axios";
+import "./CommentEditor.css"
 
 const { TextArea } = Input;
 
 const CommentList = ({ comments }) => (
   <List
+  className="commentList"
+    size="small"
     dataSource={comments}
-    header={`${comments.length} ${comments.length > 1 ? "replies" : "reply"}`}
+    header={`${comments.length} 則留言`}
     itemLayout="horizontal"
     renderItem={(props) => (
-      <>
+      <List.Item>
         <Comment
           author={props.user.name}
           content={props.comment}
           datetime={moment(props.createdAt).fromNow()}
         />
-        {/* <Divider orientation="left" className="Divider" span></Divider> */}
-      </>
+        <Space>
+          <EditOutlined />
+          <DeleteOutlined />
+        </Space>
+      </List.Item>
     )}
   />
 );
 
-const Editor = ({ onChange, onSubmit, loading, value }) => (
+const Editor = ({ onChange, onSubmit, submitting, value }) => (
   <>
     <Form.Item>
       <TextArea rows={4} onChange={onChange} value={value} />
@@ -40,7 +47,7 @@ const Editor = ({ onChange, onSubmit, loading, value }) => (
     <Form.Item>
       <Button
         htmlType="submit"
-        loading={loading}
+        loading={submitting}
         onClick={onSubmit}
         type="primary"
       >
@@ -67,7 +74,7 @@ function CommentEditor() {
         comment: value.trim(),
       });
       const { data } = send;
-      console.table(data);
+      console.log(data.err);
       setValue("");
     } catch (error) {
       throw new Error(error);
@@ -75,7 +82,6 @@ function CommentEditor() {
   };
 
   const handleChange = (e) => {
-    console.log(e.target.value);
     setValue(e.target.value);
   };
 
@@ -83,8 +89,6 @@ function CommentEditor() {
     try {
       const get = await axios.get("/api/comment");
       const { data } = get;
-      console.dir(get);
-      console.table(data);
       setComments(data.comments);
       setLoading(false);
     } catch (error) {
@@ -94,8 +98,8 @@ function CommentEditor() {
 
   useEffect(() => {
     setInterval(() => {
-      getComment()
-    }, (1000 * 5));
+      getComment();
+    }, 1000 * 5);
   }, []);
 
   useEffect(() => {
@@ -117,14 +121,15 @@ function CommentEditor() {
           paragraph={{ rows: 2 }}
           style={{ width: 500, marginTop: 10 }}
           active
-        />
+        >
+          {comments.length > 0 && <CommentList comments={comments} />}
+        </Skeleton>
         <Skeleton
           loading={loading}
           paragraph={{ rows: 1 }}
           style={{ width: 500, marginTop: 10 }}
           active
         />
-        {comments.length > 0 && <CommentList comments={comments} />}
       </Card>
       <Comment
         // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />}
