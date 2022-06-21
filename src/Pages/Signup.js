dbee5047ea85f1a944e286d66378e6c33834ea83
Jonathan <jo901025@gmail.com>
2022-06-21 11:57:20 +0800

    confirm password

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -64,7 +64,7 @@ const Signup = () => {
         <Input />
       </Form.Item>
 
-      <Row gutter={24}>
+      <Row gutter={18}>
         <Col span={24} sm={12}>
           <Form.Item
             label="密碼"
